951c6f3d6627e451ca374430ce03697267b9d67d
C-Fredrick <chrisfredrick97@gmail.com>
2017-02-27 22:54:49 -0500

    Add files via upload

---
+++ HW5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[] )
+{
+	printf("Christopher Fredrick" );
+	return EXIT_SUCCESS;
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for hw5
+
+HW5: HW5.c
+	gcc -o HW5 HW5.c 
